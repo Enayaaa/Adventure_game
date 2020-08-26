0eee738dd2d412e506c78f3782b6917052a3c238
Enayaaa <50826990+Enayaaa@users.noreply.github.com>
2020-08-26 08:12:11 +0200

    Update controller.py

--- controller.py
+++ controller.py
@@ -1,37 +1,49 @@
 import world
 import player
-import pprint
-
-# main loop
-# skapa värld med places
-# låt användaren ge input
+import sys
 
 w = world.World()
 p = player.Player(0, 0)
 
-while True:
-    print("You are at %s" % p.get_place_name(w.places))
-    print("Coordinates: (%d, %d)" % (p.x, p.y))
-    command = input("___________\nCommands:\n___________\nw - move up\ns - move down\na - move left\nd - move right\nquit - quit game\n").lower()
-    '''
-    print("Debug")
-    for i in range(len(w.places)):
-        for j in range(len(w.places[i])):
-            print(j, i, w.places[j][i].name)
-    '''
-    if command == "quit":
-        break
-    elif command == "w":
-        p.y -= 1
-    elif command == "s":
-        p.y += 1
-    elif command == "a":
-        p.x -= 1
-    elif command == "d":
-        p.x += 1
-    else:
-        continue
+print()
+print("𝕎𝕖𝕝𝕔𝕠𝕞𝕖 𝕥𝕠 𝕋𝕙𝕚𝕤 𝕒𝕕𝕧𝕖𝕟𝕥𝕦𝕣𝕖 𝕘𝕒𝕞𝕖!")
+print("_________________________________________")
+print("Ｕｓｅ ｔｈｅ ｆｏｌｌｏｗｉｎｇ ｃｏｍｍａｎｄｓ ｔｏ ｅｘｐｌｏｒｅ ｔｈｅ ｗｏｒｌｄ：")
+print(
+    "w - move up\ns - move down\na - move left\nd - move right\nlook - explore the current place"
+    "\nlisten - listen for sounds\nquit - quit game"
+)
+print("_________________________________________")
 
+
+def handle_command():
+    command = input().lower()
+
+    if command == "quit":
+        sys.exit()
+    elif command == "look":
+        print(p.look(w.places))
+    elif command == "listen":
+        print(p.listen(w.places))
+    else:
+        p.move(command)
+
+
+# Main program loop
+while True:
+    print()
+    print("Current place %s (%d, %d)" % (p.get_place_name(w.places), p.x, p.y))
+    '''
+        print("Debug")
+        for i in range(len(w.places)):
+            for j in range(len(w.places[i])):
+                print(j, i, w.places[j][i].name)
+        '''
+
+    # Handle commands
+    handle_command()
+
+    # Handle index out of range
     if p.x < 0:
         p.x = len(w.places) - 1
     if p.x > len(w.places) - 1:
