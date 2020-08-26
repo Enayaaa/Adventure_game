ac583cadc753c95e2f71d3e344ae7db05ab71ab8
Enayaaa <50826990+Enayaaa@users.noreply.github.com>
2020-08-26 08:11:26 +0200

    update world.py

--- world.py
+++ world.py
@@ -1,15 +1,15 @@
 import place
 
+
 class World:
-
     def __init__(self):
-        self.places = [[],[]]
-        self.create()
+        self.places = []
+        self.create(2)
 
-    def create(self):
-        self.places[0].append(place.Place("North West", "Cool", "Northy", "Snow white"))
-        self.places[1].append(place.Place("North East", "Chilly", "Northy", "oeof"))
-        self.places[0].append(place.Place("South West", "Sunny", "piefp", "Green trees"))
-        self.places[1].append(place.Place("South East", "Flamming hot", "persain", "Desert"))
-
-
+    def create(self, height):
+        for x in range(height):
+            self.places.append([])
+        self.places[0].append(place.Place("North West", "cool", "Norse", "snow white", "snow pattering"))
+        self.places[1].append(place.Place("North East", "chilly", "Swedish", "small bushes", "wind howling"))
+        self.places[0].append(place.Place("South West", "sunny", "Spanish", "green trees", "birds chirping"))
+        self.places[1].append(place.Place("South East", "flaming hot", "Persian", "desert", "vultures calling"))
