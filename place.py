750358a39dc488013d6a7c149421f837c3f1b857
Enayaaa <50826990+Enayaaa@users.noreply.github.com>
2020-08-26 08:12:37 +0200

    Update place.py

--- place.py
+++ place.py
@@ -1,9 +1,9 @@
 # The Datamodel
 
 class Place:
-    def __init__(self, name, weather,language, veiw):
+    def __init__(self, name, weather,language, veiw, sound):
         self.name = name
         self.weather = weather
         self.language = language
         self.veiw = veiw
-
+        self.sound = sound
